267f4e8043248eb6c115f846c5f877156c1e2e88
wangbo <2926284266@qq.com>
2020-02-15 23:20:32 +0800

    LeetCode 算法题解

--- src/LeetCode_10_IsMatch.java
+++ src/LeetCode_10_IsMatch.java
@@ -9,7 +9,7 @@ public class LeetCode_10_IsMatch {
      * 使用如下回溯算法的时间复杂度远远不如使用动态规划求解
      * 但是，目前以我对动态规划的理解和运用还做不出这道题，还要继续加油
      * */
-    public boolean isMatch(String s, String p) {
+    /*public boolean isMatch(String s, String p) {
         if(p.isEmpty()) {
             return s.isEmpty();
         }
@@ -23,5 +23,27 @@ public class LeetCode_10_IsMatch {
         }else {
             return first_match && isMatch(s.substring(1), p.substring(1));
         }
+    }*/
+
+    /**
+     * 自底向上的动态规划
+     * */
+    public boolean isMatch(String text, String pattern) {
+        boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
+        dp[text.length()][pattern.length()] = true;
+        for (int i = text.length(); i >= 0; i--) {
+            for (int j = pattern.length() - 1; j >= 0; j--) {
+                boolean first_match = false;
+                if (i < text.length() && (text.charAt(i) == pattern.charAt(j) || '.' == pattern.charAt(j))) {
+                    first_match = true;
+                }
+                if (j < pattern.length() - 1 && '*' == pattern.charAt(j + 1)) {
+                    dp[i][j] = dp[i][j + 2] || first_match && dp[i + 1][j];
+                } else {
+                    dp[i][j] = first_match && dp[i + 1][j + 1];
+                }
+            }
+        }
+        return dp[0][0];
     }
 }
